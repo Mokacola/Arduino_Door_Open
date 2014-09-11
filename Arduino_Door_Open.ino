2661e85fe56ac804d24594f5e9ec749dbfe34997
Mokacola <andrewmoeckel95@gmail.com>
2014-09-10 23:17:54 -0500

    Initial Code
    the framework of the program

---
+++ Arduino_Door_Open.ino
@@ -0,0 +1,34 @@
+// pin assignments
+ int LDR = 0;
+ int LED = 4;
+ 
+ // variables
+ int base;
+ int threshold = 40;
+ 
+ // declare inputs and outputs
+ // and take a baseline reading
+ void setup() 
+ {
+   Serial.begin(9600);
+   //pinMode(LED, OUTPUT);
+   pinMode(LDR, INPUT);
+   pinMode(LED, INPUT);
+   base = analogRead(LDR);
+ }
+ 
+
+ void loop() 
+ {
+   int v = analogRead(LDR);
+   if ((base - v) > threshold) 
+   {
+     Serial.println("Photoresistor Triggered");
+     digitalWrite(LED, HIGH);
+   } 
+   else
+   {
+     digitalWrite(LED, LOW);
+   }
+   delay(10);
+ }
